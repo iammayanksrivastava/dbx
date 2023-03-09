35e332e8a94c6015becb7310060d6e52987bb193
Mayank Srivastava <mayanksrivastava@Macbook-Pro-2.local>
2023-03-09 11:02:42 +0100

    Testing Code

---
+++ databricks-demo/databricks-training.py
@@ -0,0 +1,20 @@
+## Code to test things on databricks server. 
+
+from pyspark.sql import functions as F
+
+path_checkpoint = "dbfs:/user/mayank.srivastava@frieslandcampina.com/dbacademy/adewd/0/_checkpoints"
+
+
+def update_silver():
+    query = (spark.readStream
+             .table("bronze")
+             .withColumn("processed_time", F.current_timestamp())
+             .writeStream.option("checkpointLocation", f"{path_checkpoint}/silver")
+             .trigger(availableNow=True)
+             .table("silver")
+             )
+    
+    query.awaitTermination()
+
+
+update_silver()
